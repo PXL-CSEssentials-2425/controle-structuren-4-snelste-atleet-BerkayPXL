e5f39623fb119f597566c6a939a24d7f8547a4bd
BerkayPXL <12400152@student.pxl.be>
2024-10-11 12:06:53 +0200

    Verbetering Text

--- SnelsteAtleet/SnelsteAtleet/MainWindow.xaml.cs
+++ SnelsteAtleet/SnelsteAtleet/MainWindow.xaml.cs
@@ -16,9 +16,46 @@ namespace SnelsteAtleet
     /// </summary>
     public partial class MainWindow : Window
     {
+        string nameFastest;
+        int secondfastest = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox1.Clear();
+            timeTextBox.Clear();
+            resultTextBox.Clear();
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+              bool IsValidNumber = int.TryParse(timeTextBox.Text, out int secondcurrent);
+
+            if (IsValidNumber)
+            {
+                if (secondcurrent == 0 || secondcurrent < secondfastest)
+                {
+                    secondfastest = secondcurrent;
+                    nameFastest = nameTextBox1.Text;
+                }
+            }
+        }
+
+        private void fastestButton_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
     }
 }
+
+
+
+            
